5032f54e47d9bf807e1b9c41e5235a56683a0ecf
Valdecir RML <valdecir.rml@gmail.com>
2022-12-20 08:07:17 -0300

    estudo 2 vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <TheHeader v-if="showHeader" />
+
+  <h1 v-bind:class="classVar">Valdecir testando classe  style com v-bind</h1>
+
   <div v-show="showName">
     Nome: {{ firstName }} <br>
     Sobrenome: {{ lastName }}
@@ -18,9 +21,15 @@
          v-bind:key="obj.id"
          class="todos-item">
         {{ index }}  {{ obj.title }}
+        <img  v-if="obj.imagemSrc"
+              v-bind:src="obj.imagemSrc" />
     </div>
   </div>
-  <img alt="Vue logo" src="./assets/logo.png">
+
+  
+  <img v-bind:src="imagemSrc" v-bind:alt="imgAlt" />
+
+  <img alt="Vue logo" src="./assets/logo.png"/> 
   <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
@@ -37,6 +46,9 @@ export default {
 
   data() {
     return {
+      classVar: 'title',
+      imgAlt: 'foto de Jhon SNow',
+      imagemSrc: 'https://via.placeholder.com/150',
       showHeader: true,
       firstName: 'Jhon',
       lastName: 'Snow',
@@ -48,13 +60,15 @@ export default {
           "userId": 1,
           "id": 1,
           "title": "delectus aut autem",
-          "completed": false
+          "completed": false,
+          "imagemSrc": 'https://via.placeholder.com/150'
         },
         {
           "userId": 1,
           "id": 2,
           "title": "quis ut nam facilis et officia qui",
-          "completed": false
+          "completed": false,
+          "imagemSrc": 'https://via.placeholder.com/150'
         },
         {
           "userId": 1,
@@ -81,6 +95,11 @@ export default {
 </script>
 
 <style>
+
+.title {
+  size: 20;
+  color:blue;
+}
 .todos-item {
     background: blueviolet;
     color: #fff;
