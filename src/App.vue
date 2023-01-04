6b10f853f7818147586d5072f6acc19115b827b5
Valdecir Rml <valdecir.rml@gmail.com>
2023-01-04 15:33:39 -0300

    Ciclo de vida (life cycle) Parte 1

--- src/App.vue
+++ src/App.vue
@@ -1,75 +1,91 @@
 <template>
   <div>
-    <h2>Observadores</h2>
-    <br />
-    <br />
-    Name:
-    <input v-model="name" type="text"/>
-    <br />
+    <h1>Elemento Raiz do DOM</h1>
     {{name}}
+  <div>
+    <h2>Cliclo de Vida (life cycle)</h2>
+    <h3>Criação</h3> 
+    <h5>Prepara o componente, faz ajax, inicializa variáveis. Não tem acesso ao template (DOM)</h5> 
+    <h3>Montagem</h3> 
+    <h5>Quando vc precisa inicializar uma lib externa (ex: new Lib()) e ela precisa de acesso ao template (DOM)
+        Tem Acesso ao template (DOM)
+    </h5> 
+    <h3>Atualização</h3> 
+    <h5>Usado para debug. Tudo o que acontece no Vue</h5> 
+    <h3>Desmonstagem</h3> 
+    <h5>Desmonsta o componente. Remove tudo o que for necessário para liberar memória.
+        Ex. Lib->destroy()
+    </h5> 
+    <h3>Hooks</h3> 
+    <h4>Usando para interagir em todas estas fases.</h4>
+    <h5>beforeCreate(){} --> antes do componente ser criado</h5>
+    <h5>created(){} ---> componente foi criado</h5>
+    <h5>beforeMount(){} --->antes do componente ser montado</h5>
+    <h5>mounted(){} --->componente foi montado</h5>
+    <h5>beforeUnmount(){} --->antes do componente ser desmontado</h5>
+    <h5>unmounted(){} --->componente desmontado</h5>
+  
     <br />
     <br />
-    Nome:
-    <input v-model="user.first_name" type="text"/>
-    <br />
-    <br />
-    Sobre:
-    <input v-model="user.last_name" type="text"/>
-    <br />
-    {{user.first_name}} {{user.last_name}}
-    <br />
-    <select v-model="pageCount">
-      <option value="5">5</option>
-      <option value="10">10</option>
-      <option value="15">15</option>
-    </select>
-    <br />
-    {{pageCount}}
   </div>
+</div>
 </template>
 
 <script>
+
 export default {
   name: "App",
 
   data() {
     return {
-      name: '',
-      pageCount: '5',
-      user: {
-        first_name: '',
-        last_name: '',
-      }
+      name: "Valdecir RML"
     }
   },
 
+  //antes do componente ser criado
+  beforeCreate(){
+    console.log('beforeCreate');
+    console.log('estado:', this.name);
+    console.log('DOM:', this.$el);
+  },
+
+  //componente foi criado
+  created(){
+    console.log('created');
+    console.log('estado:', this.name);
+    console.log('DOM:', this.$el);
+  },
+
+  //antes do componente ser montado
+  beforeMount(){
+    console.log('beforeMount');
+    console.log('estado:', this.name);
+    console.log('DOM:', this.$el);
+  },
+  // componente foi montado
+  mounted(){
+    console.log('mounted');
+    console.log('estado:', this.name);
+    console.log('DOM:', this.$el);
+  },
+  //antes do componente ser desmontado
+  beforeUnmount(){
+    console.log('beforeUnmount');
+  },
+
+  // componente desmontado</h5>
+  unmounted(){
+    console.log('unmounted');
+  },
+
+
   components: {
+    
 
   },
 
   watch: {
-    name(vlr){
-      // só executa rotina que valor tiver 3 ou mais caracteres
-      if (vlr.length >= 3) {
-      this.saveUserName();
-      }
-    },
-        pageCount(){
-      this.changePage();  
-    },
-
-    /* funçãoooooooooooooooooo
-    user(){
-     console.log('usuario alterado')
-    } */
-
-    // objetoooooooooooooooo
-    user: {
-       handler(){
-        console.log('usuario alterado disparo handler')
-       },
-       deep: true
-    }
+    
   },
 
   computed: {
@@ -77,14 +93,7 @@ export default {
   },
 
   methods: {
-    saveUserName(){
-      console.log('ajax saveUserName');
-      console.log(this.name);
-    },
-    changePage(){
-      console.log('ajax changePage');
-      console.log(this.pageCount);
-    }
+    
   },
 };
 </script>
